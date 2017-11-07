fe6984263aed176dfec2edab8cbb424eed8f68bc
Mubeen Khan <mkhantx@gmail.com>
2017-11-06 21:31:27 -0600

    added more reduce operation

--- repeater/src/com/csc/test/streams/ReduceStream.java
+++ repeater/src/com/csc/test/streams/ReduceStream.java
@@ -6,11 +6,17 @@ public class ReduceStream {
 
 	public static void main(String[] args) {
 		
-		Integer[] ids = new Integer[] { 1, 3, 5,6,7,8,9,13,14,15,16,17,18,20 };
+		Integer[] ids = new Integer[] {  3, 5,6,7,8,9,13,14,15,16,17,18,20 };
 		
-		Integer result = Stream.of(ids).reduce(1, (n1,n2)->n1+n2);
-	
+		Integer result = Stream.of(ids).reduce(0, (n1,n2)->n1+n2);
 		System.out.println(result);
+
+		Integer result2 = Stream.of(ids).reduce(0, Integer::sum);
+		System.out.println(result2);
+
+		Integer result3 = Stream.of(ids).reduce(Integer.MAX_VALUE, Integer::min);
+		System.out.println(result3);
+		
 		
 		String[] strings = new String[] {"a","b","c","D","e"};
 		
